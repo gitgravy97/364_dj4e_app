20f358df91b3ae8e59c3b59a4d373ced58c0b161
McCoy <mcshaned@umich.edu>
2019-02-22 01:08:48 +0000

    Admin established, models added, migrations made, things working well so-far

--- autos/admin.py
+++ autos/admin.py
@@ -1,3 +1,6 @@
+# Register your models here
 from django.contrib import admin
+#from autos.models import Make, Auto
 
-# Register your models here.
+#admin.site.register(Make)
+#admin.site.register(Auto)
--- dj4e/urls.py
+++ dj4e/urls.py
@@ -20,4 +20,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('autos/', include('autos.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
